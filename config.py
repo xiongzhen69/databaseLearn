a23afe4a4a04c201c48f12d98a8054faf2e660cc
pythnon spider <xiongzhen69@gmail.com>
2017-05-31 16:18:14 +0800

    database

---
+++ config.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# coding=utf-8
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
